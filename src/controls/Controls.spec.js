80aa0063fa112a94e75be724488d68b8ed8cbdd7
Chris Daley <Daleys@Chriss-iMac.local>
2019-12-06 20:53:10 -0700

    got controls and dashboard to test positively rendered

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
-// Test away!
+import React from 'react';
+import {render} from '@testing-library/react';
+import'@testing-library/jest-dom';
+
+import Dashboard from "../dashboard/Dashboard";
+import Controls from './Controls';
+
+
+test('Controls renders correctly', () => {
+      const controls = render(<Controls />);
+      expect(controls).toMatchSnapshot();
+})
+
+test('should render correctly', () => {
+      const locked = jest.fn();// mock functions
+      const closed = jest.fn();
+      const toggleLocked = jest.fn();
+      const toggleClosed = jest.fn();
+      render(
+      <Controls 
+          locked={locked} 
+          closed={closed} 
+          toggleLocked={toggleLocked}
+          toggleClosed={toggleClosed}
+          />
+      );
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+
+// test('', () => {
+  
+// })
+
+test('Dashboard renders without crashing', () => {
+      render (<Dashboard />);
+  });
